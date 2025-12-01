6a8ab08444b7836833b2f4869c2cac4a7e7b669a
Alexander Mut <mutalex@gmail.com>
2025-12-01 21:53:14 +0100

    data: Update `abcfinance` contact person and title, and remove `media-in-motion` entry.

--- data/realContacts.js
+++ data/realContacts.js
@@ -1,7 +1,7 @@
 export const difficultContacts = [
 
     {
-        id: "case_abcfinance_wieland_full",
+        id: "case_abcfinance_mut_full",
         text: `
 abcfinance GmbH
 Kamekestraße 2-8
@@ -9,12 +9,12 @@ Kamekestraße 2-8
 Telefon: (0221) 579 08-0
 Telefax: (0221) 579 08-9126
 E-Mail: info(at)abcfinance.de
-Geschäftsführung:
-Simon Wieland
+Mittelstandsberater:
+Alexander Mut
   `,
         expected: {
-            fn: "Simon Wieland",
-            title: "Geschäftsführung",
+            fn: "Alexander Mut",
+            title: "Mittelstandsberater",
             org: "abcfinance GmbH",
             email: "info@abcfinance.de",
             url: "abcfinance.de",           // NEU: Domain aus der E-Mail extrahiert
@@ -24,37 +24,7 @@ Simon Wieland
         }
     },
 
-    {
-        id: "case_media_in_motion",
-        text: `
-Media in Motion e.K.
-Bargkoppelweg 72
-22145 Hamburg
-Germany
-​
-Telefon    +49 40 2262111-60
-Email      info(at)media-in-motion.com
-Web        www.media-in-motion.com
-​
-Inhaber
-Henning Heidmann
-​
-Sitz der Gesellschaft: Hamburg, Deutschland
-Registergericht: Amtsgericht Hamburg HRA 104877
-​
-UST-ID-NR: DE304114555
-  `,
-        expected: {
-            fn: "Henning Heidmann",
-            title: "Inhaber",
-            org: "Media in Motion e.K.",
-            email: "info@media-in-motion.com", // (at) fix
-            url: "www.media-in-motion.com",    // Explizite URL
-            tel: "+4940226211160",             // Formatiert
-            cell: "",                          // WICHTIG: Muss leer sein (Parser darf hier nichts erfinden)
-            adr: "Bargkoppelweg 72, 22145 Hamburg, Germany"
-        }
-    },
+
 
 
 
