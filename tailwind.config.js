2f3d6a2e556df0a215d1996d9bd683a2ed9e641e
Alexander Mut <mutalex@gmail.com>
2025-12-04 14:43:07 +0100

    chore: Refine Tailwind CSS content paths by replacing broad globs with specific directory inclusions.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./**/*.{js,ts,jsx,tsx}",
+        "./*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./hooks/**/*.{js,ts,jsx,tsx}",
+        "./utils/**/*.{js,ts,jsx,tsx}",
+        "./auth/**/*.{js,ts,jsx,tsx}",
+        "./services/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: 'class',
     theme: {
